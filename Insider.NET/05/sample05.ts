e13eedf09cfbdbc66b0dff9de32abdacc3d71bbb
tomohikoseven <tomohikoseven0905@gmail.com>
2018-07-17 07:34:51 +0900

    第５回の途中。

---
+++ Insider.NET/05/sample05.ts
@@ -0,0 +1,13 @@
+var rank: string[] = ["不可", "可", "良", "優"];
+var score, rnum: number;
+score = 75;
+if (score < 60) {
+    rnum = 0;
+} else if (score < 70) {
+    rnum = 1;
+} else if (score < 80) {
+    rnum = 2;
+} else {
+    rnum = 3;
+}
+alert("あなたの成績は"+ rank[rnum] + "です");
