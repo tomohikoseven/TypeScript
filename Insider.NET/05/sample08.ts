917f07d82d0d088df6b89c5cdcd538eec63f62db
tomohikoseven <tomohikoseven0905@gmail.com>
2018-07-21 08:26:57 +0900

    第５回。

---
+++ Insider.NET/05/sample08.ts
@@ -0,0 +1,11 @@
+var discount: number;
+var price: number = 1000;
+var rank: string = "gold";
+
+discount = (rank == "gold" ? price * 0.2 : price * 0.1);
+alert(discount);var discount: number;
+var price: number = 1000;
+var rank: string = "gold";
+
+discount = (rank == "gold" ? price * 0.2 : price * 0.1);
+alert(discount);
