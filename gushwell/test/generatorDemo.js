f5a3a83c1ccabb54e0441f9226c2aab5ecabd845
tomohikoseven <tomohikoseven0905@gmail.com>
2018-09-02 15:07:13 +0900

    gushwell test

--- gushwell/test/generatorDemo.js
+++ gushwell/test/generatorDemo.js
@@ -3,8 +3,18 @@ function* generator(i) {
     yield i;
     yield i + 10;
 }
+class Test {
+    *generator(i) {
+        yield i;
+        yield i + 10;
+    }
+}
 var gen = generator(10);
 for (let g of gen) {
+    console.log(g);
+}
+let tgen = Test.generator(10);
+for (let g of tgen) {
     console.log(g);
 }
 //console.log(gen.next().value);
--- gushwell/test/generatorDemo.ts
+++ gushwell/test/generatorDemo.ts
@@ -2,12 +2,23 @@ function* generator(i:number) {
   yield i;
   yield i + 10;
 }
+class Test {
+  public static function* generator(i:number) {
+    yield i;
+    yield i + 10;
+  }
+}
 
 var gen = generator(10);
 
 for( let g of gen )
 {
     console.log(g);
+}
+
+let tgen = Test.generator(10);
+for( let g of tgen ){
+  console.log(g);
 }
 
 //console.log(gen.next().value);
